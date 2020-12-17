c2e7f2dbca66fca2d66de435bfd482750624a526
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2020-12-17 10:47:54 -0800

    finishing up PUT and DEL endpoints along with reqparser

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'drew'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='this field cant bbe left blank')
 
+    @jwt_required()
     def get(self, name):
-        item = next(filter(lambda item: item['name'] == item, items), None)
+        item = next(filter(lambda x: x['name'] == name, items), None)
         # next gives us the first item in this filter function
+        # placing None in next(), gives the function a default
 
         # for item in items:
         #     if item['name'] == name:
@@ -19,10 +32,28 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+        if next(filter(lambda item: item['name'] == item, items), None):
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
